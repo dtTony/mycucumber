fb535a8ba40a20cd41db719c9ff41f433c9c6f94
dingtaotony <dingtaotony@didiglobal.com>
2019-06-26 18:12:28 +0800

    update test

---
+++ src/main/java/indi/tony/auto/base/BaseTest.java
@@ -0,0 +1,41 @@
+package indi.tony.auto.base;
+
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+import org.testng.annotations.AfterSuite;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseTest extends AbstractTestNGCucumberTests {
+    @AfterSuite
+    public static void afterTest(){
+        File reportOutputDirectory = new File("target");
+        List<String> jsonFiles = new ArrayList<String>();
+        jsonFiles.add("target/cucumber-json-report.json");
+
+        String buildNumber = "1";
+        String projectName = "cucumberProject";
+//        boolean runWithJenkins = false;
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+// optional configuration - check javadoc
+//        configuration.setRunWithJenkins(runWithJenkins);
+        configuration.setBuildNumber(buildNumber);
+// addidtional metadata presented on main page
+        configuration.addClassifications("Platform", "Windows");
+        configuration.addClassifications("Browser", "Firefox");
+        configuration.addClassifications("Branch", "release/1.0");
+
+// optionally add metadata presented on main page via properties file
+//        List<String> classificationFiles = new ArrayList<String>();
+//        classificationFiles.add("properties-1.properties");
+//        configuration.addClassificationFiles(classificationFiles);
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        Reportable result = reportBuilder.generateReports();
+    }
+}
